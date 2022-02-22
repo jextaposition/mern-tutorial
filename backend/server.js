84759432d3dd625103eed28bb96168abdba23e29
Alexa Hayes <alexa.joy.hayes@gmail.com>
2022-02-22 12:01:27 -0800

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
-const port = process.env.PORT || 8000;
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5001;
+
+connectDB();
 
 const app = express();
 
